05c9f1f89dd6ba2239a135d6e8273bd6bd3fe64a
Christian Westrom <c.westrom@westrom.xyz>
2025-01-18 15:01:19 +0900

    change all formatting

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,12 @@
-use std::process::Command;
 use std::io::ErrorKind;
+use std::process::Command;
 
 #[cfg(feature = "compiled-map")]
 // Look for any .blp files in the project (ignoring target and git dirs)
-// 
+//
 // This function is recursive, and calls itself when it hits a directory. The `path`
 // argument should be "." in the starting call.
-// 
+//
 // Returns a Vec with the paths of all the .blp files to be compiled.
 pub fn search_blps(start_path: &str) -> Vec<String> {
     let mut paths = Vec::<String>::new();
@@ -17,11 +17,11 @@ pub fn search_blps(start_path: &str) -> Vec<String> {
         let entry_name = dir_entry.file_name();
         let entry_name = match entry_name.to_str() {
             Some(entry_name) => entry_name,
-            None => continue // name cannot be converted to utf8 string
+            None => continue, // name cannot be converted to utf8 string
         };
         let entry_type = match dir_entry.file_type() {
             Ok(entry_type) => entry_type,
-            Err(_) => continue
+            Err(_) => continue,
         };
 
         // Ignore ".git" and "target" directories
@@ -30,9 +30,8 @@ pub fn search_blps(start_path: &str) -> Vec<String> {
             for path in search_blps(&format!("{start_path}/{entry_name}")) {
                 paths.push(path);
             }
-        }
-        else if ( entry_type.is_file() || entry_type.is_symlink() ) &&
-            entry_name.strip_suffix(".blp").is_some()
+        } else if (entry_type.is_file() || entry_type.is_symlink())
+            && entry_name.strip_suffix(".blp").is_some()
         {
             // Found Blueprint file
             paths.push(format!("{start_path}/{entry_name}"));
@@ -42,15 +41,14 @@ pub fn search_blps(start_path: &str) -> Vec<String> {
     paths
 }
 
-
 /// Uses the installed blueprint-compiler python script to compile `.blp` files to
 /// `UI XML` that can be used by *GtkBuilder*. The compiler needs to be accessible
 /// through the **$PATH** envirnoment variable or it needs to be in
 /// "blueprint-compiler/blueprint-compiler.py" in the Project Root.
 pub fn compile_blp(path: &str) -> Result<String, String> {
     /* These are commands that the function could use as the compiler. The ones with
-       the "./" prefix are in the $PATH environment variable, and the ones that have
-       that prefix are relative to the current project's root directory. */
+    the "./" prefix are in the $PATH environment variable, and the ones that have
+    that prefix are relative to the current project's root directory. */
     let possible_compilers = [
         // Rank by which one is more likely
         &mut Command::new("blueprint-compiler"),
@@ -60,38 +58,45 @@ pub fn compile_blp(path: &str) -> Result<String, String> {
     ];
 
     /* Try to find the right compiler in one of the above locations.
-       Use whichever one is successful */
+    Use whichever one is successful */
     for compiler in possible_compilers {
         compiler.arg("compile");
         compiler.arg(path);
 
         /* The output contains the command's exit status, stdout, and stderr.
-           When Command::output() returns an Err, it means that the command could
-           not run for some reason */
+        When Command::output() returns an Err, it means that the command could
+        not run for some reason */
         let output = match compiler.output() {
             Ok(output) => output,
-            Err(error) =>
+            Err(error) => {
                 if error.kind() == ErrorKind::NotFound {
                     // Try another command
-                    continue
+                    continue;
                 } else {
-                    return Err(format!("Unknown error occurred while invoking compiler:\n{}", error))
+                    return Err(format!(
+                        "Unknown error occurred while invoking compiler:\n{}",
+                        error
+                    ));
                 }
+            }
         };
 
         let compiled_blp = String::from_utf8(output.stdout).unwrap();
         let error = String::from_utf8(output.stderr).unwrap();
-        
+
         if output.status.success() {
-            return Ok(compiled_blp)
+            return Ok(compiled_blp);
         } else {
             // When blueprint-compiler reaches an error in the blueprint file's source code
             // it will exit with 1 and the error info in stdout. Other errors will be written
             // to stderr. To show all errors, return Err Result with both stdout and stderr
             return Err(match output.status.code() {
-                Some(code) => format!("blueprint-compiler exit code: {}\n{}\n{}", code, compiled_blp, error),
-                None => format!("{}\n{}", compiled_blp, error)
-            })
+                Some(code) => format!(
+                    "blueprint-compiler exit code: {}\n{}\n{}",
+                    code, compiled_blp, error
+                ),
+                None => format!("{}\n{}", compiled_blp, error),
+            });
         }
     }
 
